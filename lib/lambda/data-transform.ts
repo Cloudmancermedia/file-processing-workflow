155d7cd1a221b7a850760f682324411e57c5ef92
Ryan Williams <ryan.j.williams@charter.com>
2024-08-05 15:07:41 -0600

    workflow up and running

--- lib/lambda/data-transform.ts
+++ lib/lambda/data-transform.ts
@@ -4,11 +4,11 @@ import { StepFunctionError } from './shared/errors';
 
 // Helper function to format data for DynamoDB
 const formatForDynamoDB = (item: ExtractedData) => {
-  const formattedItem: { [key: string]: { S?: string; N?: number } } = {};
+  const formattedItem: { [key: string]: { S?: string; N?: string } } = {};
 
   for (const key in item) {
     if (typeof item[key] === 'number') {
-      formattedItem[key] = { N: item[key] };
+      formattedItem[key] = { N: item[key].toString() };
     } else if (typeof item[key] === 'string') {
       formattedItem[key] = { S: item[key].toLowerCase() }; // Convert to lowercase
     }
